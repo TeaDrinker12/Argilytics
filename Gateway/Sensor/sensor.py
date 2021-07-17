2c996ea2bc3ea32043a45722aa0aa8da90a7b28c
= <81148886+TeaDrinker12@users.noreply.github.com>
2021-07-17 16:32:11 +0300

    Add initial temp function

--- Gateway/Sensor/sensor.py
+++ Gateway/Sensor/sensor.py
@@ -1,11 +1,14 @@
 import random
 import time
 
+def initialTemprature():
+    return round(random.uniform(0, 35), 2)
+
 def adjust(temprature):
     adjustment = random.choice([-0.03, 0.00, 0.03])
     return temprature + adjustment
 
-temprature = round(random.uniform(0, 35), 2)
+temprature = initialTemprature()
 while True:
     time.sleep(1)
     temprature = adjust(temprature)
