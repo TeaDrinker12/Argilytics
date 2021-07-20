a56210847812266d13918cdd303a5bc31abf8c1f
= <81148886+TeaDrinker12@users.noreply.github.com>
2021-07-20 16:54:24 +0300

    Add accept reading data

--- Gateway API/server.js
+++ Gateway API/server.js
@@ -3,7 +3,17 @@ app = express();
 
 app.use(express.json());
 app.post("/reading", (req, res) => {
-    return res.status(404).json(req.body);
+    if (!req.body.timestamp) {
+        return res.status(500).json({fail: "Timestamp not present"});
+    }
+    if (!req.body.temprature) {
+        return res.status(500).json({fail: "Temprature not present"});
+    }
+    const reading = {
+        timestamp: new Date(req.body.timestamp),
+        temprature: req.body.temprature,
+    }
+    return res.status(201).json(reading);
 });
 app.use((req, res) => {
     return res.status(404).json({
